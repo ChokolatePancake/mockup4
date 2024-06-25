bb884f44258d942a769d80320a2c6c5e28191af4
Solya <catsubrosa@gmail.com>
2024-06-25 15:30:20 +0300

    Add testimonials

---
+++ src/js/testimonials-slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.testimonials__content').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        speed:500,
+        fade: true,
+        autoplaySpeed: 10000,
+        infinite:true
+    });
+});
