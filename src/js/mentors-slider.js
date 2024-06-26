8546b2dbb8f435ac7551a9560dcb809f9661ac9d
Solya <catsubrosa@gmail.com>
2024-06-26 14:32:12 +0300

    Add mentors block

---
+++ src/js/mentors-slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.mentors__content').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        speed:500,
+        autoplaySpeed: 10000,
+        infinite:true,
+        dots: true
+    });
+});
